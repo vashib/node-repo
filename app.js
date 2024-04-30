fec95b62cf2970a74ad3e211f099873f1f780131
vashib <156540010+vashib@users.noreply.github.com>
2024-04-30 21:19:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(3000, () => console.log('Server ready'))
